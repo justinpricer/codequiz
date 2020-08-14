7bd51c483a3945cb671268652f03f99dde6a231b
Justin Pricer <justin.pricer@gmail.com>
2020-08-13 20:23:40 -0500

    update

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var questions = [
     {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        title: "What is the HTML tag under which one can write the JavaScript code?",
+        choices: ["<javascript>", "<scripted>", "<script>", "<js>"],
+        answer: "<script>"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        title: "The external JavaScript file must contain <script> tag. True or False?",
+        choices: ["True", "False"],
+        answer: "False"
     },
     {
         title: "Arrays in Javascript can be used to store ____.",
